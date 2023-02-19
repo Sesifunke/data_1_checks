b4006dab43bff7554d07c97695608c8a370f9a80
Busayo Olusesi <funkeolusesi@aol.com>
2023-02-19 13:49:11 -0500

    added source code hello world

---
+++ hello.py
@@ -0,0 +1,45 @@
+##- KNOWLEDGE CHECK -CODE LOUISVILLE
+
+#Step 1- A  PRINT STATEMENT
+
+print("hello world!")
+
+x = 100
+firstname = 'wonder'
+lastname = 'John'
+wonderland =' We are going to florida'
+
+print(wonderland) 
+
+
+"""
+Knowledge Check - Step 2
+List popuLated with sveral values
+"""
+#- STEP 2- THIS IS A LIST - LISTS ARE IDENTIFIED WITH []
+
+firstFruit, secondFruit, thirdFruit ="Bannana", "Cherry", "Mango"
+
+
+print(firstFruit)
+print(secondFruit)
+
+favFruit = ["Orange", "Pineapple", "Grapes"]
+a,b,c = favFruit
+
+print(a)
+print(b)
+print(c)
+
+#STEP 3-DICTIONARY POPULATED
+
+print(type(favFruit))
+
+
+# STEP 4 - A TUPLE WITH 4 VALUES
+#TUPLE ARE IDENTIFIED BY ()
+
+FoundFruits = ("Blueberry", "Pear", "Peach", "Dragon Fruit")
+A,B,C,D = FoundFruits
+
+print(D)
